8b4452bd7d65a21080dd3dbaebb9d9e198945d4c
Arnab Khan <arnkhan@deloitte.com>
2025-07-21 00:50:06 +0530

    Stream API

--- src/main/java/Java8/StreamAPIExample/Client.java
+++ src/main/java/Java8/StreamAPIExample/Client.java
@@ -23,7 +23,8 @@ public class Client {
         /**
          * Group The employees By Department Names
          */
-        Map<String, List<Employee>> collect = employees.stream().collect(Collectors.groupingBy(emp -> emp.getDepartNames()));
+        Map<String, List<Employee>> collect = employees.stream()
+                                                        .collect(Collectors.groupingBy(emp -> emp.getDepartNames()));
         System.out.println("Group The employees By Department Names " + collect);
 
         /**
@@ -39,68 +40,115 @@ public class Client {
         /***
          * 	Find all department names
          */
-        List<String> departmentList = employees.stream().map(emp -> emp.getDepartNames()).distinct().collect(toList());
+        List<String> departmentList = employees.stream()
+                                                .map(emp -> emp.getDepartNames())
+                                                .distinct()
+                                                .collect(toList());
         System.out.println(departmentList);
 
         /**
          * 	Find the count of employee in each department
          */
-        System.out.println(employees.stream().collect(Collectors.groupingBy(Employee::getDepartNames,Collectors.counting())));
+        System.out.println(
+                employees.stream()
+                        .collect(Collectors.groupingBy(Employee::getDepartNames, Collectors.counting()))
+        );
 
         /**
          * Find the list of employees whose age is less than 30
          */
-        List<Employee> list = employees.stream().filter(emp -> emp.getAge() < 30).toList();
+        List<Employee> list = employees.stream()
+                .filter(emp -> emp.getAge() < 30)
+                .toList();
         System.out.println(list);
 
         /**
          * 	Find the list of employees whose age is in between 26 and 31
          */
-        List<Employee> list1 = employees.stream().filter(employee -> employee.getAge() > 26 && employee.getAge() <31).toList();
+        List<Employee> list1 = employees.stream()
+                .filter(employee -> employee.getAge() > 26 && employee.getAge() < 31)
+                .toList();
         System.out.println(list1);
 
         /**
          * Find the average age of male and female employee
          */
-        System.out.println(employees.stream().
-                collect(Collectors.groupingBy(Employee::getGender,Collectors.averagingInt(Employee::getAge))));
+        System.out.println(
+                employees.stream().
+                collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingInt(Employee::getAge)))
+        );
 
         /**
          * Find the department who is having maximum number of employee
          */
 
-        System.out.println(employees.stream().collect(Collectors.groupingBy(Employee::getDepartNames,Collectors.counting()))
-                .entrySet().stream().max(Map.Entry.comparingByValue()).get());
+        System.out.println(
+                employees.stream()
+                        .collect(Collectors.groupingBy(Employee::getDepartNames, Collectors.counting()))
+                        .entrySet()
+                        .stream()
+                        .max(Map.Entry.comparingByValue())
+                        .get()
+        );
 
         /**
-         * Find the Employee who stays in Delhi and sort them by their names
+         * Find the Employee who stays in Chennai and sort them by their names
          */
 
-        System.out.println(employees.stream().filter(emp -> emp.getAddress().equals("Chennai"))
-                .sorted(Comparator.comparing(Employee::getName)).toList());
+        System.out.println(
+                employees.stream()
+                .filter(emp -> emp.getAddress()
+                        .equals("Chennai"))
+                        .sorted(Comparator.comparing(Employee::getName))
+                        .toList());
 
         /**
          * Find the average salary in all departments
          */
-        System.out.println(employees.stream().collect(Collectors.groupingBy(Employee::getDepartNames
-                ,Collectors.averagingDouble(Employee::getSalary))));
+        System.out.println(
+                employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartNames, Collectors.averagingDouble(Employee::getSalary))));
 
         /**
          * Find the highest salary in each department
          */
-        System.out.println(employees.stream().collect(Collectors.groupingBy(Employee::getDepartNames,
-                Collectors.maxBy(Comparator.comparing(Employee::getSalary)))));
+        System.out.println(
+                        employees.stream()
+                                .collect(Collectors.groupingBy(Employee::getDepartNames, Collectors
+                                .maxBy(Comparator.comparing(Employee::getSalary))))
+        );
 
         /**
          * Find the list of employee and sort them by their salary
          */
 
-        System.out.println(employees.stream().sorted(Comparator.comparing(Employee::getSalary)).toList());
+        System.out.println(
+                employees.stream()
+                        .sorted(Comparator.comparing(Employee::getSalary))
+                        .toList()
+        );
 
         /**
-         * Find the employee who has second lowest salary
+         * Find the employee who has second-lowest salary
          */
-        System.out.println(employees.stream().sorted(Comparator.comparing(Employee::getSalary)).skip(1).findFirst().get());
+        System.out.println(
+                employees.stream()
+                        .sorted(Comparator.comparing(Employee::getSalary))
+                        .skip(1)
+                        .findFirst()
+                        .get()
+        );
+
+        /**
+         * Find the employee who has second-highest salary
+         */
+        System.out.println(
+                employees.stream()
+                        .sorted(Comparator.comparing(Employee::getSalary).reversed())
+                        .skip(1)
+                        .findFirst()
+                        .get()
+        );
 
 
     }
